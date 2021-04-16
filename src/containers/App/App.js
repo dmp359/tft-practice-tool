7b518abf2d6474933db4c9709fcfb7d0e8613f1f
Dan Perlman <daniel@insituware.com>
2021-04-16 00:53:25 -0400

    sample game interaction

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,16 +1,20 @@
-import React, { useState } from 'react';
-import background from 'images/far-away.jpg';
-import GameContainer from 'containers/GameContainer';
+import React, { useState } from "react";
+import background from "images/far-away.jpg";
+import GameContainer from "containers/GameContainer";
 
-import './App.css';
+import "./App.css";
 
 function App() {
   const [start, setStart] = useState(false);
   return (
     <div className="App">
-      <header className='App-header' style={{ backgroundImage: `url(${background})` }}>
-      {!start && <button className="Begin-button" onClick={() => setStart(true)}>Begin (Set 4)</button>}
-      {start && <GameContainer />}
+      <header className="App-header" style={{ backgroundImage: `url(${background})` }}>
+        {!start && (
+          <button className="Begin-button" onClick={() => setStart(true)}>
+            Begin (Set 4)
+          </button>
+        )}
+        {start && <GameContainer />}
       </header>
     </div>
   );
--- src/containers/GameContainer/GameContainer.jsx
+++ src/containers/GameContainer/GameContainer.jsx
@@ -1,23 +1,58 @@
-import React from 'react';
-import ChampionCard from 'components/ChampionCard';
-import './GameContainer.css';
+import React, { useState } from "react";
+import ChampionCard from "components/ChampionCard";
+import "./GameContainer.css";
 
 /**
  * Handle game state
  */
 function GameContainer() {
-  return (
-    <div className="container">
-      <div className="grid">
-      <ChampionCard />
-      <ChampionCard />
-      <ChampionCard />
-      <ChampionCard />
+  // Sample data
+  const aatrox = {
+    id: 1,
+    name: "Aatrox",
+    img: "https://fastcdn.mobalytics.gg/assets/tft/images/champions/thumbnail/set4/aatrox.jpg",
+    traits: [
+      {
+        name: "Cultist",
+        img: "https://fastcdn.mobalytics.gg/assets/tft/images/synergies/set4/cultist-gold.svg",
+      },
+      {
+        name: "Vanguard",
+        img: "https://fastcdn.mobalytics.gg/assets/tft/images/synergies/set4/vanguard-gold.svg",
+      },
+    ],
+    cost: 4,
+  };
+  // Display 3 cards of aatroxes
+  const [cards, setCards] = useState([aatrox, { ...aatrox, id: 2 }, { ...aatrox, id: 3 }]);
 
+  // Callback for on click of card
+  const onCardClick = (id) => {
+    // For now, just delete the card from the state
+    setCards(cards.filter((card) => id !== card.id));
+  };
+
+  return (
+    <>
+      <div className="container">
+        <div className="grid">
+          {cards.map(({ id, name, traits, img, cost }) => (
+            <ChampionCard
+              onClick={onCardClick}
+              id={id}
+              name={name}
+              traits={traits}
+              img={img}
+              cost={cost}
+            />
+          ))}
+        </div>
+        {cards.length < 1 && (
+          <div style={{ backgroundColor: "black", height: "48px" }}>You Win!</div>
+        )}
       </div>
-    </div>
-    
-  )
+    </>
+  );
 }
 
-export default GameContainer; 
+export default GameContainer;
